6612edc67b0ae7bc078149118e77a685c0bcd354
dshiv <deepak.shivalingu@gmail.com>
2020-06-22 01:52:24 +0530

    deepak commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,10 +4,6 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-<<<<<<< HEAD
-        System.out.println("i have learnt on how to integrate Poll SCM with Jenkins"); 
-=======
-        System.out.println("Hello i did some changes"); 
->>>>>>> af4541061bfcd60be9b1cef7a5df69a55daf7547
+        System.out.println("deepak Shivalingu"); 
     } 
 } 
